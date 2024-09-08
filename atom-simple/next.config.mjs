c742487c79a68d67ea4115e371e37b175431081e
sibulut-std <sibulut@yahoo.com>
2024-09-07 22:08:10 -0400

    Major changes

--- atom-simple/next.config.mjs
+++ atom-simple/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  // Remove the experimental.appDir option
+}
 
 export default nextConfig;
