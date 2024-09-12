246b13e878cde6c20d74ccf6a3fb0ce8816285cf
sibulut-std <sibulut@yahoo.com>
2024-09-12 12:42:24 -0400

    Add name field tot he sign up form

--- atom-simple/app/sign.tsx
+++ atom-simple/app/sign.tsx
@@ -9,6 +9,7 @@ export default function Auth() {
   const [isSignUp, setIsSignUp] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
   const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const router = useRouter();
@@ -22,7 +23,7 @@ export default function Auth() {
         }
       } catch (error) {
         // If there's no current user, getCurrentAuthenticatedUser will throw an error
-        // We can ignore this error as it means there's no user to sign out
+        // Ignore this error as it means there's no user to sign out
       } finally {
         setIsLoading(false);
       }
@@ -32,7 +33,7 @@ export default function Auth() {
   }, []);
 
   const validateForm = () => {
-    if (!email || !password) {
+    if (!email || !password || (isSignUp && !name)) { // Validate name if signing up
       setError('Please fill in all fields.');
       return false;
     }
@@ -66,8 +67,7 @@ export default function Auth() {
 
       // After successful authentication, try to get user metadata
       try {
-        // Use email as username
-        await getUserMetadata(email);
+        await getUserMetadata(email, name); // Pass both email and name
       } catch (dbError) {
         if (dbError instanceof Error) {
           console.error('DynamoDB error:', dbError);
@@ -121,6 +121,23 @@ export default function Auth() {
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
+            {isSignUp && ( // Add this block to include the name input only during sign up
+              <div className="mb-4">
+                <label htmlFor="name" className="sr-only">
+                  Name
+                </label>
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+            )}
             <div className="mb-4">
               <label htmlFor="password" className="sr-only">
                 Password
